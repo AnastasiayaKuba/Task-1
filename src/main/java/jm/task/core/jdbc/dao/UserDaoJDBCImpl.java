6239c590f7b08410d07065690c7604d0fb29c7dd
AnastasiayaKuba <anastasia.kubanskaia.01@gmail.com>
2024-12-21 20:55:36 +0300

    Версия 1, без правок

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,113 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
-
     }
 
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE IF NOT EXISTS users (" +
+                "id SERIAL PRIMARY KEY, " +
+                "name VARCHAR(255), " +
+                "lastName VARCHAR(255), " +
+                "age SMALLINT)";
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            System.out.println("Произошла ошибка при создании таблицы.");
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS users;";
+        try (Connection connection = Util.getConnection();
+        Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+            System.out.println("Таблица «пользователи» успешно удалена.");
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        if (name == null || lastName == null) {
+            throw new IllegalArgumentException("Имя и фамилия не могут быть null");
+        }
+        String sql = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows > 0) {
+                System.out.println("Пользователь успешно сохранен.");
+            } else {
+                System.out.println("Не удалось сохранить пользователя.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
+        String sql = "DELETE FROM users WHERE id = ?";
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            int affectedRows = preparedStatement.executeUpdate();
 
+            if (affectedRows > 0) {
+                System.out.println("Пользователь с ID" + id + "успешно удален.");
+            } else {
+                System.out.println("Пользователь с идентификатором не найден" + id + ".");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+        try (Connection connection = Util.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet rs = statement.executeQuery()) {
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String lastName = rs.getString("lastName");
+                byte age = rs.getByte("age");
+                User user = new User(name, lastName, age);
+                long id = rs.getLong("id");
+                if (id != 0) {
+                    users.add(user);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
     }
 
-    public void cleanUsersTable() {
+        public void cleanUsersTable() {
+        String sql = "TRUNCATE TABLE users";
+            try (Connection connection = Util.getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.execute();
+                System.out.println("Таблица 'users' была успешно очищена.");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
 
-    }
+        }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,44 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        Connection conn = null;
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String username = "postgres";
+        String pass = "1984";
+        try {
+            conn = DriverManager.getConnection(url, username, pass);
+            System.out.println("Подключение к базе данных выполнено успешно!");
+        } catch (SQLException e) {
+            System.out.println("Ошибка подключения к базе данных");
+            throw new RuntimeException(e);
+        }
+        return conn;
+    }
+
+    public static Session getSession() {
+        Session session = null;
+        try {
+            Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+            SessionFactory sessionFactory = configuration.buildSessionFactory();
+            session = sessionFactory.getCurrentSession();
+            System.out.println("Подключение к базе данных выполнено успешно!");
+        } catch (Exception e) {
+            System.out.println("Ошибка подключения к базе данных");
+        }
+        return session;
+    }
+
 }
+
+
